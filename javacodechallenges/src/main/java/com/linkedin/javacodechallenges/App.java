a79836682e6d0743302bfcbd189f8f8be3269085
alvaromunozhonorato <121574125+alvaromunozhonorato@users.noreply.github.com>
2022-12-28 18:49:22 +0000

    pass complexity

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,11 +1,42 @@
 package com.linkedin.javacodechallenges;
 
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 public class App {
 
     public static boolean isPasswordComplex(String password) {
-        return false;
+
+        // 6 caracteres
+        // 1 uppercase
+        // 1 lowercase
+        // 1 number
+        int countuppercase = 0;
+        int countlowercase = 0;
+        int length = 0;
+        int countnumbers = 0;
+
+        char[] chararray = password.toCharArray();
+
+        for (char c : chararray) {
+            boolean uppercase = Character.isUpperCase(c);
+            if (uppercase)
+                countuppercase++;
+            else
+                countlowercase++;
+            
+            if(Character.isDigit(c))
+                countnumbers ++; 
+        }
+
+        length = password.length();
+
+        if ((length >= 6) && (countlowercase > 0) && (countuppercase > 0) && (countnumbers > 0) ) {
+            return true;
+        } else {
+            return false;
+        }
+
     }
 
     public static void main(String[] args) {
